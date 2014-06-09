28448d42346d5f240de00a8cbe6bba9ed9438e17
Nick Kenyeres <nkenyeres@gmail.com>
2014-06-08 23:45:46 -0400

    Changed mail settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'www.carlinreport.com' }
+  config.action_mailer.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'www.carlinreport.com',
+      :authentication => :plain
+  }
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
